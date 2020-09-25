d2b2ab2dfc60551a0bb0b88bb80790d288724d61
BoKeJoR <jorgecarmarquez@gmail.com>
2020-09-25 11:26:05 +0200

    Primer Commit

---
+++ server/config/config.js
@@ -0,0 +1,4 @@
+/*=============================================
+El process es un objeto global que corre en todo el entorno de desarrollo de nodeJS
+=============================================*/
+process.env.PORT = process.env.PORT || 3000;
